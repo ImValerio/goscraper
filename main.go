cb1a84fb9e064dc181246e0339fc9087f6503221
ImValerio <valeriovallettadev@gmail.com>
2024-05-04 14:13:53 +0200

    refactoring, save all tokens and read thorugh it

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 
 	"github.com/go-chi/chi/middleware"
@@ -39,7 +41,7 @@ func main() {
 
 	router.Post("/scrape", scrapeUrl)
 
-	http.ListenAndServe(":5000", router)
+	http.ListenAndServe("localhost:5000", router)
 }
 
 func scrapeUrl(w http.ResponseWriter, r *http.Request) {
@@ -60,7 +62,7 @@ func scrapeUrl(w http.ResponseWriter, r *http.Request) {
 
 	for _, miner := range miners {
 		wg.Add(1)
-		miner.scrapeUrl(&wg)
+		miner.scrapeUrl(&wg, req.Tags)
 	}
 
 	wg.Wait()
@@ -69,7 +71,7 @@ func scrapeUrl(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(miners)
 }
 
-func (m *Miner) scrapeUrl(wg *sync.WaitGroup) {
+func (m *Miner) printTokens(wg *sync.WaitGroup, tags []string) {
 	defer wg.Done()
 	res, err := http.Get(m.Url)
 
@@ -78,26 +80,16 @@ func (m *Miner) scrapeUrl(wg *sync.WaitGroup) {
 	}
 
 	tkz := html.NewTokenizer(res.Body)
-	for {
-		//get the next token type
+
+	counter := 0
+
+	for counter < 100 {
+
+		// tokenBefore := tkz.Token()
 		tokenType := tkz.Next()
-
+		token := tkz.Token()
 		if tokenType == html.StartTagToken {
-			token := tkz.Token()
-
-			if token.Data == "title" {
-				for tokenType != html.TextToken {
-					tokenType = tkz.Next()
-
-				}
-				if tokenType == html.TextToken {
-					content := tkz.Token().Data
-					// fmt.Println("CONTENT:", content)
-
-					m.Res = append(m.Res, content)
-				}
-
-			}
+			fmt.Println("-> ", token.Data)
 		}
 		if tokenType == html.ErrorToken {
 			err := tkz.Err()
@@ -106,6 +98,106 @@ func (m *Miner) scrapeUrl(wg *sync.WaitGroup) {
 				break
 			}
 			log.Fatalf("error tokenizing HTML: %v", tkz.Err())
+		}
+		counter++
+	}
+
+}
+
+func (m *Miner) scrapeUrl(wg *sync.WaitGroup, tags []string) {
+	defer wg.Done()
+	res, err := http.Get(m.Url)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	tkz := html.NewTokenizer(res.Body)
+
+	var tokens []*html.Token
+
+	for {
+
+		tokenType := tkz.Next()
+		token := tkz.Token()
+
+		if tokenType == html.ErrorToken {
+			err := tkz.Err()
+			if err == io.EOF {
+				//end of the file, break out of the loop
+				break
+			}
+			log.Fatalf("error tokenizing HTML: %v", tkz.Err())
+		}
+		tokens = append(tokens, &token)
+
+	}
+	fmt.Println("found ", len(tokens), " tokens")
+
+	index := 0
+	prevIndex := 0
+	counter := 0
+
+	for index < len(tokens)-1 {
+
+		token := tokens[index]
+
+		if token.Type != html.StartTagToken {
+			index++
+			continue
+		}
+
+		if token.Data == tags[counter] {
+
+			if counter == 0 {
+				prevIndex = index + 1
+			}
+			// fmt.Println("----> ", token.Data, token.ype)
+			if counter == len(tags)-1 {
+
+				innerIndex := index
+				for token.Type != html.TextToken {
+
+					if innerIndex+1 > len(tokens)-1 {
+						fmt.Println("BREAK")
+						counter = 0
+						index = prevIndex
+						break
+					}
+					token = tokens[innerIndex]
+					innerIndex++
+				}
+
+				if innerIndex+1 > len(tokens)-1 {
+					continue
+				}
+				content := token.Data
+				content = strings.ReplaceAll(content, "\\n", "\n")
+				content = strings.ReplaceAll(content, "\\t", "\t")
+
+				// Trim leading and trailing whitespace
+				content = strings.TrimSpace(content)
+				fmt.Println("CONTENT:", content)
+				if content != "" {
+					m.Res = append(m.Res, content)
+				}
+				counter = 0
+				index = prevIndex
+				continue
+
+			} else {
+				index++
+				counter++
+				continue
+			}
+
+		} else {
+			if counter == 0 {
+				index++
+				continue
+			}
+			index = prevIndex
+			counter = 0
 		}
 
 	}
