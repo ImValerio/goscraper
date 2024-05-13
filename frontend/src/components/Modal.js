743a180223f860b220aa00197152e69fd8eac793
ImValerio <valerio.valletta@icloud.com>
2024-05-13 21:29:41 +0200

    fix modal style

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Modal = ({ data, setShowModal }) => {
   return (
-    <div className="modal">
-      <div className="w-full flex justify-end ">
+    <div className="modal max-w-sm min-w-sm flex justify-between flex-col">
+      <div className="w-full flex justify-end">
         <button
           className="text-xl font-bold px-2 py-1 bg-red-500 hover:bg-red-700 text-white transition-all ml-1"
           onClick={() => setShowModal(false)}
@@ -11,9 +11,11 @@ const Modal = ({ data, setShowModal }) => {
           X
         </button>
       </div>
-      <div className="text-xl">{data.urls}</div>
+      <div className="p-5">
+        <div className="text-xl">{data.urls}</div>
 
-      <div className="text-xl text-slate-400">{data.pattern}</div>
+        <div className="text-xl text-slate-400">{data.pattern}</div>
+      </div>
     </div>
   );
 };
